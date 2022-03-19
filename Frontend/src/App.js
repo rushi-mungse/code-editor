ed8ebf2bf61ba654b03f0dc724a786a938cb9890
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-19 11:53:54 +0530

    home page designing

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import EditorPage from "./Pages/EditorPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/editor/:id" element={<EditorPage />} />
       </Routes>
     </Router>
   );
--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,5 +1,25 @@
 const Home = () => {
-  return <div>Home components</div>;
+  return (
+    <div className="homePageWrapper flexStyle">
+      <div className="formWrapper flexStyle">
+        <h1 className="heading">Code Editor</h1>
+        <p className="mainLabel">Paste Invitation Room Id</p>
+        <input type="text" placeholder="ROOM ID" className="inputTag" />
+        <input type="text" placeholder="User Name" className="inputTag" />
+        <button className="btn btnJoin">JOIN</button>
+        <p className="inviteBtn">
+          If you don't have an invite then create &nbsp;
+          <a href="" target={"_blank"}>
+            New Room
+          </a>
+        </p>
+      </div>
+      <footer className="footer">
+        Built with 🖤 by
+        <a href="https://github.com/rushi-mungse"> Rushikesh Mungse</a>
+      </footer>
+    </div>
+  );
 };
 
 export default Home;
