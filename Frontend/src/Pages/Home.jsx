40b64e00fcf4bf1756ab29784e74e2d015ffd076
Rushikesh Mungse <86994311+rushi-mungse@users.noreply.github.com>
2022-03-19 17:45:04 +0530

    adding react hot toast

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,15 +1,63 @@
+import { v4 as uuidV4 } from "uuid";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+
+  const createNewUuid = (e) => {
+    e.preventDefault();
+    const id = uuidV4();
+    setRoomId(id);
+    toast.success("Creating new room id.", { position: "top-right" });
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      return toast.error("Please fill all the fields.", {
+        position: "top-right",
+      });
+    }
+    navigate(`/editor/${roomId}`);
+    return toast.success("You entering in room.", {
+      position: "top-right",
+    });
+  };
+
+  const handleInputEvent = (e) => {
+    if (e.code === "Enter") return joinRoom();
+  };
+
   return (
     <div className="homePageWrapper flexStyle">
       <div className="formWrapper flexStyle">
         <h1 className="heading">Code Editor</h1>
         <p className="mainLabel">Paste Invitation Room Id</p>
-        <input type="text" placeholder="ROOM ID" className="inputTag" />
-        <input type="text" placeholder="User Name" className="inputTag" />
-        <button className="btn btnJoin">JOIN</button>
+        <input
+          type="text"
+          placeholder="ROOM ID"
+          className="inputTag"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+          onKeyUp={(e) => handleInputEvent(e)}
+        />
+        <input
+          type="text"
+          placeholder="User Name"
+          className="inputTag"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          onKeyUp={(e) => handleInputEvent(e)}
+        />
+        <button className="btn btnJoin" onClick={joinRoom}>
+          JOIN
+        </button>
         <p className="inviteBtn">
           If you don't have an invite then create &nbsp;
-          <a href="" target={"_blank"}>
+          <a href="" target={"_blank"} onClick={(e) => createNewUuid(e)}>
             New Room
           </a>
         </p>
